bcbab18ca1752321de8b43364d0d66101528e53e
Nabeel Ahmed <chaudhrynabeelahmed@gmail.com>
2017-07-16 01:15:46 +0500

    minor edits

--- tweets_positive.py
+++ tweets_positive.py
@@ -4,41 +4,39 @@ from tweepy import OAuthHandler
 from textblob import TextBlob
  
 class TwitterClient(object):
-    '''
-    Generic Twitter Class for sentiment analysis.
-    '''
-    def __init__(self, consumer_key=None, consumer_secret=None, access_token=None, access_token_secret=None):
-        '''
-        Class constructor or initialization method.
-        '''
+    """
+        Generic Twitter Class for sentiment analysis.
+    """
+    def __init__(self, key=None, secret=None, token=None, token_secret=None):
+        """
+            Class constructor or initialization method.
+        """
         # keys and tokens from the Twitter Dev Console
-        if consumer_key and consumer_secret and access_token and access_token_secret:
-			# attempt authentication
-			try:
-				# create OAuthHandler object
-				self.auth = OAuthHandler(consumer_key, consumer_secret)
-				# set access token and secret
-				self.auth.set_access_token(access_token, access_token_secret)
-				# create tweepy API object to fetch tweets
-				self.api = tweepy.API(self.auth)
-			except:
-				print("Error: Authentication Failed")
-		else:
-			print("Error: Consumer key, secret and access token are required for authentication")
+        if key and secret and token and token_secret:
+            # attempt authentication
+            try:
+                # create OAuthHandler object
+                self.auth = OAuthHandler(key, secret)
+                # set access token and secret
+                self.auth.set_access_token(token, token_secret)
+                # create tweepy API object to fetch tweets
+                self.api = tweepy.API(self.auth)
+            except Exception, err_msg:
+                print("Error: Authentication Failed \n %s" % err_msg)
+        else:
+            print("Error: Consumer key, secret and access token are required for authentication")
  
     def clean_tweet(self, tweet):
-        '''
+        """
         Utility function to clean tweet text by removing links, special characters
         using simple regex statements.
-        '''
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])
-                                    |(\w+:\/\/\S+)", " ", tweet).split())
+        """
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
  
     def get_tweet_sentiment(self, tweet):
-        '''
-        Utility function to classify sentiment of passed tweet
-        using textblob's sentiment method
-        '''
+        """
+            Utility function to classify sentiment of passed tweet using textblob's sentiment method
+        """
         # create TextBlob object of passed tweet text
         analysis = TextBlob(self.clean_tweet(tweet))
         # set sentiment
@@ -50,25 +48,29 @@ class TwitterClient(object):
             return 'negative'
  
     def get_tweets(self, keyword, count = 10):
-        '''
+        """
         Main function to fetch tweets and parse them.
-        '''
+
+        :params
+            keyword - the search keyword, you need to search tweets for.
+            count - no of tweets to fetch from the search
+        """
         # empty list to store parsed tweets
         tweets = []
  
         try:
             # call twitter api to fetch tweets
-            fetched_tweets = self.api.search(q = query, count = count)
- 
+            fetched_tweets = self.api.search(q=keyword, count=count)
+
             # parsing tweets one by one
             for tweet in fetched_tweets:
-                # empty dictionary to store required params of a tweet
-                parsed_tweet = {}
+                # saving text and sentiment
+                parsed_tweet = {'text': tweet.text, 'sentiment': self.get_tweet_sentiment(tweet.text)}
  
                 # saving text of tweet
-                parsed_tweet['text'] = tweet.text
+                # parsed_tweet['text'] = tweet.text
                 # saving sentiment of tweet
-                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+                # parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
  
                 # appending parsed tweet to tweets list
                 if tweet.retweet_count > 0:
@@ -87,15 +89,15 @@ class TwitterClient(object):
  
     
 if __name__ == "__main__":
-	# keys and tokens from the Twitter Dev Console
-	consumer_key = raw_input('Enter/Paste the consumer key')
-	consumer_secret = raw_input('Enter/Paste the consumer secret')
-	access_token = raw_input('Enter/Paste the access token')
-	access_token_secret = raw_input('Enter/Paste the access token secret')
+    # keys and tokens from the Twitter Dev Console
+    consumer_key = raw_input('Enter/Paste the consumer key')
+    consumer_secret = raw_input('Enter/Paste the consumer secret')
+    access_token = raw_input('Enter/Paste the access token')
+    access_token_secret = raw_input('Enter/Paste the access token secret')
     # creating object of TwitterClient Class
     api = TwitterClient(consumer_key, consumer_secret, access_token, access_token_secret)
     # calling function to get tweets
-	keyword = raw_input('Enter the keyword - to search the tweets')
+    keyword = raw_input('Enter the keyword - to search the tweets')
     tweets = api.get_tweets(keyword=keyword, count=200)
  
     # picking positive tweets from tweets
